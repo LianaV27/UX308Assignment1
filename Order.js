43b970c1f3ae96f94617eae133f8296da32274ec
LianaV27 <144968889+LianaV27@users.noreply.github.com>
2025-03-01 05:39:17 +0000

    add final order message

--- Order.js
+++ Order.js
@@ -4,8 +4,7 @@ export class Order {
       WELCOMING: () => {
         let aReturn = [];
         this.stateCur = this.OrderState.ORDERING;
-        aReturn.push("Welcome to Liana's Tea Shop.");
-        aReturn.push("What kind of drink would you like today?");
+        aReturn.push("Welcome to Liana's Tea Shop. What kind of drink would you like today?");
         aReturn.push("<h2>Drink Menu</h2><ul><li>Milk Tea</li> <li>Fruit Tea</li> </ul>");
         return aReturn;
       },
@@ -18,8 +17,6 @@ export class Order {
         }
         else if (sInput.toLowerCase().startsWith('f')) {
           let drink = 'fruit tea';
-          aReturn.push("Thanks for trying our reservation system");
-          aReturn.push("Maybe next time");
         }
         // SET SIZE
         aReturn.push("What size would you like?");
