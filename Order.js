b6d989f216aff749bc86b05db0fc486916523e06
LianaV27 <144968889+LianaV27@users.noreply.github.com>
2025-03-01 05:21:51 +0000

    add toppings & upsell

--- Order.js
+++ Order.js
@@ -1,44 +1,67 @@
 export class Order {
-    constructor(sFrom) {
-      this.OrderState = {
-        WELCOMING: () => {
-          let aReturn = [];
-          this.stateCur = this.OrderState.ORDERING;
-          aReturn.push("Welcome to Liana's Tea Shop.");
-          aReturn.push("What kind of drink would you like today?");
-          aReturn.push("<h2>Drink Menu</h2><ul><li>Milk Tea</li> <li>Fruit Tea</li> </ul>");
-          return aReturn;
-        },
-        ORDERING: (sInput) => {
-          let aReturn = [];
-          this.isDone = true;
-          if (sInput.toLowerCase().startsWith('m')) {
-            let drink = 'milk tea';
-            aReturn.push("What size would you like?");
-            aReturn.push("<ul><li>Large</li> <li>Medium</li>");
-            aReturn.push("What topping would you like?");
-            aReturn.push("<ul><li>Boba</li> <li>Jelly</li>");
-            aReturn.push("you have ordered" + drink);
-          } 
-          else if (sInput.toLowerCase().startsWith('s')){
-            aReturn.push("Thanks for trying our reservation system");
-            aReturn.push("Maybe next time");
-          } else {
-            aReturn.push("Thanks for trying our reservation system");
-            aReturn.push("Maybe next time");
-          }
-          return aReturn;
+  constructor(sFrom) {
+    this.OrderState = {
+      WELCOMING: () => {
+        let aReturn = [];
+        this.stateCur = this.OrderState.ORDERING;
+        aReturn.push("Welcome to Liana's Tea Shop.");
+        aReturn.push("What kind of drink would you like today?");
+        aReturn.push("<h2>Drink Menu</h2><ul><li>Milk Tea</li> <li>Fruit Tea</li> </ul>");
+        return aReturn;
+      },
+      ORDERING: (sInput) => {
+        let aReturn = [];
+        this.isDone = true;
+        // SET DRINK
+        if (sInput.toLowerCase().startsWith('m')) {
+          let drink = 'milk tea';
         }
-      };
-  
-      this.stateCur = this.OrderState.WELCOMING;
-      this.isDone = false;
-      this.sFrom = sFrom;
-    }
-    handleInput(sInput) {
-      return this.stateCur(sInput);
-    }
-    isDone() {
-      return this.isDone;
-    }
+        else if (sInput.toLowerCase().startsWith('f')) {
+          let drink = 'fruit tea';
+          aReturn.push("Thanks for trying our reservation system");
+          aReturn.push("Maybe next time");
+        }
+        // SET SIZE
+        aReturn.push("What size would you like?");
+        aReturn.push("<ul><li>Large</li> <li>Regular</li>");
+        if (sInput.toLowerCase().startsWith('l')) {
+          let size = 'large';
+        }
+        else if (sInput.toLowerCase().startsWith('r')) {
+          let size = 'regular';
+        }
+
+        // SET TOPPING
+        aReturn.push("What topping would you like?");
+        aReturn.push("<ul><li>Tapioca</li> <li>Jelly</li>");
+        if (sInput.toLowerCase().startsWith('t')) {
+          let size = 'tapioca';
+        }
+        else if (sInput.toLowerCase().startsWith('j')) {
+          let size = 'jelly';
+        }
+
+        // SET UPSELL
+        aReturn.push("Would you like a bubble waffle with that for $3?");
+        aReturn.push("<ul><li>Large</li> <li>Regular</li>");
+        if (sInput.toLowerCase().startsWith('y')) {
+          let upsell = ' and a bubble waffle.';
+        }
+        else {
+          let upsell = '.';
+        }
+        aReturn.push("you have ordered a " + size + " " + drink + " with " + topping + upsell);
+      }
+    };
+
+    this.stateCur = this.OrderState.WELCOMING;
+    this.isDone = false;
+    this.sFrom = sFrom;
   }
+  handleInput(sInput) {
+    return this.stateCur(sInput);
+  }
+  isDone() {
+    return this.isDone;
+  }
+}
