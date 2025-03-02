b924d83c8cfdd60c1d5e8e34ff16be5331e6941b
LianaV27 <144968889+LianaV27@users.noreply.github.com>
2025-03-02 03:17:25 +0000

    restructuring code

--- Order.js
+++ Order.js
@@ -4,62 +4,60 @@ export class Order {
       WELCOMING: () => {
         let aReturn = [];
         this.stateCur = this.OrderState.ORDERING;
-        aReturn.push("Welcome to Liana's Tea Shop. What kind of drink would you like today?");
-        aReturn.push("<h2>Drink Menu</h2><ul><li>Milk Tea</li> <li>Fruit Tea</li> </ul>");
+        aReturn.push("Welcome to Rich's Acton Rapid Test.");
+        aReturn.push("Would you like to reserve a rapid test kit?");
         return aReturn;
       },
       ORDERING: (sInput) => {
         let aReturn = [];
-        this.isDone = true;
-        //aReturn.push("you have ordered a " + drink)
-        //aReturn.push("you have ordered a " + $size + " " + $drink + " with " + topping + upsell);
-        // SET DRINK
-        setDrink;
-        function setDrink(){
-          let drink;
-          if (sInput.toLowerCase().startsWith('m')) {
-            drink = 'milk tea';
-          }
-          else if (sInput.toLowerCase().startsWith('f')) {
-            drink = 'fruit tea';
-          }
-          return drink;
-        }
-        console.log(`drink is ${setDrink('m')}`);
-      }
-    }
-  }
-}
-/*
-        // SET SIZE
-        aReturn.push("What size would you like?");
-        aReturn.push("<ul><li>Large</li> <li>Regular</li>");
-        if (sInput.toLowerCase().startsWith('l')) {
-          let size = 'large';
-        }
-        else if (sInput.toLowerCase().startsWith('r')) {
-          let size = 'regular';
-        }
-        // SET TOPPING
-        aReturn.push("What topping would you like?");
-        aReturn.push("<ul><li>Tapioca</li> <li>Jelly</li>");
-        if (sInput.toLowerCase().startsWith('t')) {
-          let size = 'tapioca';
-        }
-        else if (sInput.toLowerCase().startsWith('j')) {
-          let size = 'jelly';
-        }
-
-        // SET UPSELL
-        aReturn.push("Would you like a bubble waffle with that for $3?");
-        aReturn.push("<ul><li>Large</li> <li>Regular</li>");
+        this.stateCur = this.OrderState.TOPPINGS;
         if (sInput.toLowerCase().startsWith('y')) {
-          let upsell = ' and a bubble waffle.';
+          aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
+          let d = new Date();
+          d.setMinutes(d.getMinutes() + 120);
+          aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
+        } else {
+          aReturn.push("Thanks for trying our reservation system");
+          aReturn.push("Maybe next time");
         }
-        else {
-          let upsell = '.';
-        }
-       
+        return aReturn;
+      },
+      TOPPINGS: (sInput) => {
+        let aReturn = [];
+        this.stateCur = this.OrderState.UPSELLING;
+        if (sInput.toLowerCase().startsWith('c')) {
+         aReturn.push(`cool indeed`);
+          aReturn.push(`thanks!!`);
+        } else {
+          aReturn.push("Thanks for trying our reservation system");
+          aReturn.push("Maybe next time");
+       }
+        return aReturn;
+      },
+      UPSELLING: (sInput) => {
+        let aReturn = [];
+        this.stateCur = this.OrderState.CONFIRMING;
+        if (sInput.toLowerCase().startsWith('c')) {
+         aReturn.push(`cool indeed`);
+          aReturn.push(`thanks!!`);
+        } else {
+          aReturn.push("Thanks for trying our reservation system");
+          aReturn.push("Maybe next time");
+       }
+        return aReturn;
+      },
+
+      CONFIRMING: (sInput) => {
+        let aReturn = [];
+        this.isDone = true;
+        if (sInput.toLowerCase().startsWith('c')) {
+         aReturn.push(`cool indeed`);
+          aReturn.push(`thanks!!`);
+        } else {
+          aReturn.push("Thanks for trying our reservation system");
+          aReturn.push("Maybe next time");
+       }
+        return aReturn;
       }
     };
 
@@ -73,4 +71,4 @@ export class Order {
   isDone() {
     return this.isDone;
   }
-}*/
+}
